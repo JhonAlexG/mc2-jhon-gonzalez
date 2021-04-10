4226c3a5448d29da75ee9d21c62c49af5a31be6a
JhonAlexG <jhonalexgonzalez1903@gmail.com>
2021-04-10 10:06:22 -0500

    Taller 14

---
+++ Taller 14/Taller 14/Taller 14/Operaciones.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller_14
+{
+    class Operaciones
+    {
+        public void Gauss(double[,] matrix, int filas, int columnas)
+        {
+            for (int fpivot = 0; fpivot < filas; fpivot++)
+            {
+                double nor = matrix[fpivot, fpivot];
+ 
+                for (int i = 0; i < columnas; i++)
+                {
+                    matrix[fpivot, i] = matrix[fpivot, i] / nor;
+                }
+ 
+                int f = fpivot + 1;
+                if (f == filas) f = 0;
+ 
+                for (int fila = 0; fila < filas - 1; fila++)
+                {                
+                    double k = matrix[f, fpivot];
+ 
+                    for (int j = fpivot; j < columnas; j++)
+                    {
+                        matrix[f, j] = matrix[f, j] - (k * matrix[fpivot, j]);
+                    }
+                 
+                    if (f == filas - 1) f = 0;
+                    else f++; 
+                }
+            }
+            
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.Write("[" + matrix[i, j] + "]");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Taller 14/Taller 14/Taller 14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operaciones Operations = new Operaciones();
+            int Filas = 3;
+            int Columnas = 4;
+            double[,] Matriz = new double[Filas, Columnas];
+            Console.WriteLine("Coloque los números de la matriz");
+            for (int i = 0;  i < Filas; i++)
+            {
+                for (int j = 0; j < Columnas; j++)
+                {
+                    Matriz[i, j] = double.Parse(Console.ReadLine());
+                }
+            }
+
+            Operations.Gauss(Matriz, Filas, Columnas);
+            Console.ReadKey();
+        }
+    }
+} 
